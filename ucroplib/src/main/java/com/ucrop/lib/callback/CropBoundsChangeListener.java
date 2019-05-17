7ea6c650d79d42debaa561081f4137526244f2d5
1193003531 <1193003531@qq.com>
2019-05-17 17:29:37 +0800

    替换图片选择框架

---
+++ ucroplib/src/main/java/com/ucrop/lib/callback/CropBoundsChangeListener.java
@@ -0,0 +1,10 @@
+package com.ucrop.lib.callback;
+
+/**
+ * Interface for crop bound change notifying.
+ */
+public interface CropBoundsChangeListener {
+
+    void onCropAspectRatioChanged(float cropRatio);
+
+}
